accb03ed66dd4c2983f45fd845fa05e8f8cb1757
Devendra Deo <this.devendra@gmail.com>
2019-01-02 23:36:14 +0530

    added code for super keyword and inheritance with super keyword calling lifeCycle methods as well with super keyword as well as setting the satet of the parent using super keyword in child

---
+++ src/parent.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+class Label extends React.Component{
+    constructor(props){
+      super(props);
+      this.className='plain-label';
+      this.state = {name:"john"};
+    }
+
+    callLabelTest(){
+        alert("here");
+    }
+
+    componentDidMount(){
+        console.log("calling component did mount..........");
+    }
+
+     render(){
+       return (
+                <span className={this.className}>
+                {this.props.children} 
+                </span>
+       )
+     }
+  }
+  
+
+  
+class SuccessLabelWithIcon extends Label{
+    constructor(props){
+        super(props);
+        this.className = this.className + ' success-label';
+        this.callLabelParent = this.callLabelParent.bind(this);
+    }
+
+    callLabelParent(){
+        super.componentDidMount();
+        super.setState({name: "Devedra"}); //we can call a method with super as well as setState with super keyword
+        super.callLabelTest();
+    }
+
+    render(){
+        return(
+            <div onClick={this.callLabelParent}>
+                <h1> name in the state is : {this.state.name}</h1>
+                {super.render()}<span>&#9650;</span>            
+            </div> 
+        )
+    }
+}
+  
+
+  
+  
+export default  class Main extends React.Component{
+
+      render(){
+          return(
+                <div>
+                    <Label> Plain Label </Label>
+                    <hr/>                    
+                    <SuccessLabelWithIcon> Success Label with Icon </SuccessLabelWithIcon>
+                </div>
+          )  
+    }
+  
+  }
